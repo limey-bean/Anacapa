8844a298aad19ce77948e6b78fa7f47683702d2e
Emily Curd <eecurd@g.ucla.edu>
2017-09-15 07:19:16 -0700

    Add files via upload

---
+++ Anacapa_db/scripts/anacapa_config.sh
@@ -0,0 +1,37 @@
+# Config file for CRUX_release_V1		09-01-2017
+# Written by Emily Curd (eecurd@g.ucla.edu) and Gaurav Kandlikar (gkandlikar@ucla.edu)
+# Developed at UCLA for the University of California Conservation Consortium's CALeDNA Program
+
+
+
+#############################
+# Paths to programs / load programs
+#############################
+
+MODULE_SOURCE="source /u/local/Modules/default/init/bash" 	#if none, leave empty <- for HPC
+SCRIPTS="script=./scripts"									#if have program scripts in a specific folder
+
+### if not loading modules, need a way to not not run lines ~65 - 70 (module load bit...)
+
+#load cutadapt
+CUTADAPT="/u/local/apps/python/2.7.13/bin/cutadapt" 		#path to cutadapt binary. see documentation for how to obtain this script
+
+#load pear
+PEAR="module load pear"									#or what ever code is used to load pear in a bash shell, or path to pear
+
+#load fastx_toolkit
+FASTX_TOOLKIT="module load fastx_toolkit"				#or what ever code is used to load fastx_toolkit in a bash shell, or path to fastx_toolkit 
+
+#load anaconda/python2-4.2
+ANACONDA_PYTHON2-4.2="module load anaconda/python2-4.2"				#or what ever code is used to load anaconda/python2-4.2 in a bash shell, or path to anaconda/python2-4.2 
+
+#load bowtie2
+BOWTIE2="module load bowtie2"							#or what ever code is used to load bowtie2 in a bash shell, or path to bowtie2 
+
+#load ATS
+ATS="module load ATS"									#or what ever code is used to load ATS in a bash shell, or path to ATS.  ATS is a Hoffman2 module that allows the user to submit a job on the HPC from within a shell script 
+
+#load Qiime
+QIIME="module load qiime" 								#or what ever code is used to load qiime in a bash shell (e.g. on a mac it might be "macqiime")
+
+
