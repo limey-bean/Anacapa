2e5834db5211d722e146d540ca72afe81fa5a99b
Emily Curd <eecurd@g.ucla.edu>
2017-09-15 20:36:46 -0700

    updated for new Anacapa script

--- Anacapa_db/scripts/pick_open_otus_and_summ.sh
+++ Anacapa_db/scripts/pick_open_otus_and_summ.sh
@@ -19,16 +19,15 @@ module load qiime
 	echo "Pick_open_reference: mapping merged-reads to refs"
 	echo "Up next $1"
 	echo "assembled started"
-	pick_open_reference_otus.py -i $2/primer_split/assembled/$1_F.fasta -o $2/Qiime_open_ref/$1/$1_assembled_open_ref_$4 -r $3/$1/$1_qiime.fasta -p $2/Qiime_open_ref/Params/Params$1_pick_open.txt --min_otu_size=1 -m uclust -s $4 --prefilter_percent_id=0.0 --suppress_align_and_tree -a 
+	pick_open_reference_otus.py -i $2/primer_sort/assembled/$1_all.clean_assembled.fasta -o $2/Qiime_open_ref/$1/$1_assembled_open_ref_$4 -r $3/$1/$1_qiime.fasta -p $2/Qiime_open_ref/Params/Params$1_pick_open.txt --min_otu_size=1 -m uclust -s $4 --prefilter_percent_id=0.0 --suppress_align_and_tree -a 
 	echo "assembled completed"
 	echo "unassembled started"
-	pick_open_reference_otus.py -i $2/primer_split/unassembled_forward/$1_F.fasta,$2/primer_split/unassembled_reverse/$1_R.fasta -o $2/Qiime_open_ref/$1/$1_unassembled_open_ref_$4 -r $3/$1/$1_qiime.fasta -p $2/Qiime_open_ref/Params/Params$1_pick_open.txt --min_otu_size=1 -m uclust -s $4 --prefilter_percent_id=0.0 --suppress_align_and_tree -a 
+	pick_open_reference_otus.py -i $2/primer_sort/unassembled_F/$1_pear_unassembled_F.fasta,$2/primer_sort/unassembled_R/$1_pear_unassembled_R.fasta -o $2/Qiime_open_ref/$1/$1_unassembled_open_ref_$4 -r $3/$1/$1_qiime.fasta -p $2/Qiime_open_ref/Params/Params$1_pick_open.txt --min_otu_size=1 -m uclust -s $4 --prefilter_percent_id=0.0 --suppress_align_and_tree -a 
 	date
-	echo "unassembled completed"
-
+	echo "unassembled completed"  
 	echo "discarded started"
-	cat $2/primer_split/discarded/$1_F.fasta $2/primer_split/discarded/$1_R.fasta >> $2/primer_split/discarded/$1.fasta
-	pick_open_reference_otus.py -i $2/primer_split/discarded/$1.fasta -o $2/Qiime_open_ref/$1/$1_discarded_open_ref_$4 -r $3/$1/$1_qiime.fasta -p $2/Qiime_open_ref/Params/Params$1_pick_open.txt --min_otu_size=1 -m uclust -s $4 --prefilter_percent_id=0.0 --suppress_align_and_tree -a 
+	cat $2/primer_sort/discarded_F/$1_all.discarded_F.fasta $2/primer_sort/discarded_R/$1_all.discarded_R.fasta >> $2/primer_sort/discarded_F/$1.fasta
+	pick_open_reference_otus.py -i $2/primer_sort/discarded_F/$1.fasta -o $2/Qiime_open_ref/$1/$1_discarded_open_ref_$4 -r $3/$1/$1_qiime.fasta -p $2/Qiime_open_ref/Params/Params$1_pick_open.txt --min_otu_size=1 -m uclust -s $4 --prefilter_percent_id=0.0 --suppress_align_and_tree -a 
 	date
 
 	echo "discarded completed"
