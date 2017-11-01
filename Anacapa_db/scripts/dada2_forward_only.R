3615a4e83592f01edcf29bb333fe82128841867e
Emily Curd <eecurd@g.ucla.edu>
2017-11-01 14:53:56 -0700

    Create dada2_forward_only.R

---
+++ Anacapa_db/scripts/dada2_forward_only.R
@@ -0,0 +1,125 @@
+#!/usr/bin/env Rscript
+
+##### command arguments for running script!
+
+ args = commandArgs(trailingOnly=TRUE)
+
+barC = arg[1]  #barcode target
+path = arg[2]  #path to the fastq files
+
+#barC = "CO1"
+#path <- paste("/Users/limeybean/Downloads/unpaired_1/",barC,sep="")
+
+
+############################################################################################Forward Reads reads
+
+library(dada2); packageVersion("dada2")
+library("seqRFLP")
+
+####################
+### Show where reads are kept
+####################
+
+list.files(path)
+
+####################
+### Filter and Trim
+####################
+
+# Sort ensures forward/reverse reads are in same order
+fnFo <- sort(list.files(path, pattern="_Paired_1_singletons.fastq"))
+# Extract sample names, assuming filenames have format: SAMPLENAME_XXX.fastq
+sample.names <- sapply(strsplit(fnFo, "_Paired_1_singletons.fastq"), `[`, 1)
+# Specify the full path to the fnFo and fnRs
+fnFo <- file.path(path, fnFo)
+
+# look at plots
+plotQualityProfile(fnFo[1:2])
+
+filt_path <- file.path(path, "filtered") # Place filtered files in filtered/ subdirectory
+filtFos <- file.path(filt_path, paste0(sample.names, "_F_filto.fastq.gz"))
+
+
+out <- filterAndTrim(fnFo, filtFos, minLen = 70,
+                     maxN=0, maxEE=c(2), truncQ=0, rm.phix=TRUE,
+                     compress=TRUE, multithread=TRUE) # On Windows set multithread=FALSE
+head(out)
+
+####################
+### learn errors
+####################
+
+errFo <- learnErrors(filtFos, multithread=TRUE)
+plotErrors(errFo, nominalQ=TRUE)
+
+####################
+### dereplicate
+####################
+
+derepFso <- derepFastq(filtFos, verbose=TRUE)
+# Name the derep-class objects by the sample names
+names(derepFso) <- sample.names
+
+####################
+### sample inference
+####################
+
+dadaFos <- dada(derepFso, err=errFo, multithread=TRUE)
+dadaFos[[1]]
+
+
+
+####################
+### Make Sequence table
+####################
+seqtabFo <- makeSequenceTable(derepFso)
+dim(seqtabFo)
+table(nchar(getSequences(seqtabFo)))
+
+####################
+### Remove Chimeras
+####################
+
+seqtabFo.nochim <- removeBimeraDenovo(seqtabFo, method="consensus", multithread=TRUE, verbose=TRUE)
+dim(seqtabFo.nochim)
+
+sum(seqtabFo.nochim)/sum(seqtabFo)
+
+####################
+### Track Reads
+####################
+
+getN <- function(x) sum(getUniques(x))
+track <- cbind(out, sapply(dadaFos, getN), rowSums(seqtabFo), rowSums(seqtabFo.nochim))
+colnames(track) <- c("input", "filtered", "denoised", "tabled", "nonchim")
+rownames(track) <- sample.names
+head(track)
+
+###########################################
+### make output files for the merged reads
+############################################
+
+# make variables, and file paths for output
+forward_barC =  paste("forward_", barC , sep='')
+forwardbarCseqnum = paste("forward_", barC , "_seq_number", sep = '')
+nochime_f_fname.fasta = paste(path,"/", "nochim_forward",barC,".fasta", sep='')
+nochime_f_fname.txt = paste(path,"/", "nochim_forward",barC,".txt", sep='')
+
+#modify table so that there are unique specific names for each read, and that also include sample realtive abundance.
+makes.sense.seqtabFo.nochim <- t(seqtabFo.nochim)
+makes.sense.seqtabFo.nochim <- cbind(seqeunces = rownames(makes.sense.seqtabFo.nochim), makes.sense.seqtabFo.nochim)
+rownames(makes.sense.seqtabFo.nochim) <- NULL
+makes.sense.seqtabFo.nochim <- as.data.frame(makes.sense.seqtabFo.nochim)
+makes.sense.seqtabFo.nochim$seqnum <- 1:nrow(makes.sense.seqtabFo.nochim) 
+namevector <- c(forward_barC)
+makes.sense.seqtabFo.nochim[ , namevector] <- forward_barC
+makes.sense.seqtabFo.nochim[[forwardbarCseqnum]] <- paste(makes.sense.seqtabFo.nochim[[forward_barC]] , makes.sense.seqtabFo.nochim$seqnum,sep="_")
+makes.sense.seqtabFo.nochim$seqnum <- NULL
+makes.sense.seqtabFo.nochim[[forward_barC]] <- NULL
+
+# final data transfermations, and export of reads as fasta files and accompanying data table.
+nochim_forward  <- makes.sense.seqtabFo.nochim[,c(which(colnames(makes.sense.seqtabFo.nochim)==forwardbarCseqnum),which(colnames(makes.sense.seqtabFo.nochim)!=forwardbarCseqnum))]
+nochim_forward_seq <- data.frame(nochim_forward[[forwardbarCseqnum]],nochim_forward$seqeunces)
+nochim_forward_seq.fasta = dataframe2fas(nochim_forward_seq, file= nochime_f_fname.fasta)
+write.table(nochim_forward, file = nochime_f_fname.txt, row.names=FALSE, sep="\t", quote=FALSE)
+
