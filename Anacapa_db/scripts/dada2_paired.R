9909f4f069da1659b8f88374f08ffe1e00ced2f4
Gaurav <gaurav.kandlikar@gmail.com>
2017-11-03 11:50:22 -0700

    updates to dada2 script

--- Anacapa_db/scripts/dada2_paired.R
+++ Anacapa_db/scripts/dada2_paired.R
@@ -9,13 +9,25 @@
 # path = args[2]  #path to the fastq files
 # barC_length = args[3] # expected seq length of the barcode.
 
-barC = "CO12"
+barC = "CO1"
 #path <- paste("/Users/limeybean/Downloads/paired/",barC,sep="")
 barC_length = "500"
 
-# Install packages that are not currently installed
+# Install packages that are not currently installed -----
+# This chunk may need attention, putting it here as a starting point - gsk
+.cran_packages  <-  c("ggplot2", "plyr", "dplyr","seqRFLP", "reshape2", "tibble")
+.bioc_packages <- c("phyloseq", "genefilter", "impute", "Biostrings", "dada2")
 
+.inst <- .cran_packages %in% installed.packages()
+if (any(!.inst)) {
+  install.packages(.cran_packages[!.inst], repos = "http://cran.rstudio.com/")
+}
 
+.inst <- .bioc_packages %in% installed.packages()
+if (any(!.inst)) {
+  source("http://bioconductor.org/biocLite.R")
+  biocLite(.bioc_packages[!.inst])
+}
 ####################################################################################### process paired end reads
 
 library("dada2")
@@ -26,6 +38,7 @@ library("Biostrings")
 library("reshape2")
 library("dplyr")
 library("tibble")
+library("ggplot2")
 ####################
 ### Show where reads are kept
 ####################
@@ -45,9 +58,10 @@ sample.names <- sapply(strsplit(fnFs, "_Paired"), `[`, 1)
 fnFs <- file.path(path, fnFs)
 fnRs <- file.path(path, fnRs)
 
-# look at plots
-plotQualityProfile(fnFs[1:2])
-plotQualityProfile(fnRs[1:2])
+# save plots in a plots directory
+dir.create(path = paste0(path, "/plots"))
+plotQualityProfile(fnFs[1:2]) + ggsave(filename = paste0(path, "/plots/forward_qualities.pdf"))
+plotQualityProfile(fnRs[1:2]) + ggsave(filename = paste0(path, "/plots/reverse_qualities.pdf"))
 
 filt_path <- file.path(path, "filtered") # Place filtered files in filtered/ subdirectory
 filtFs <- file.path(filt_path, paste0(sample.names, "_F_filt.fastq.gz"))
@@ -56,7 +70,7 @@ filtRs <- file.path(filt_path, paste0(sample.names, "_R_filt.fastq.gz"))
 
 out <- filterAndTrim(fnFs, filtFs, fnRs, filtRs, minLen = 70,
                      maxN=0, maxEE=c(2,2), truncQ=0, rm.phix=TRUE,
-                     compress=TRUE, multithread=TRUE) # On Windows set multithread=FALSE
+                     compress=F, multithread=F) # On Windows set multithread=FALSE
 head(out)
 
 ####################
@@ -65,8 +79,8 @@ head(out)
 
 errF <- learnErrors(filtFs, multithread=TRUE)
 errR <- learnErrors(filtRs, multithread=TRUE)
-plotErrors(errF, nominalQ=TRUE)
-plotErrors(errR, nominalQ=TRUE)
+plotErrors(errF, nominalQ=TRUE) + ggsave(filename = paste0(path, "/plots/forward_error_profile.pdf"))
+plotErrors(errR, nominalQ=TRUE) + ggsave(filename = paste0(path, "/plots/reverse_error_profile.pdf"))
 
 ####################
 ### dereplicate
@@ -116,18 +130,21 @@ cat("Proportion of sequences kept after chimera removal: "); cat(sum(seqtab.noch
 ### Track Reads
 ####################
 
-getN <- function(x) sum(getUniques(x))
-track <- cbind(out, sapply(dadaFs, getN), sapply(mergers, getN), rowSums(seqtab), rowSums(seqtab.nochim))
-colnames(track) <- c("input", "filtered", "denoised", "merged", "tabled", "nonchim")
+getN <- function(x) {sum(getUniques(x))}
+track <- data.frame(input = out[,"reads.in"], filtered = out[,"reads.out"], 
+                    denoised =sapply(dadaFs, getN), 
+                    merged = sapply(mergers, getN), 
+                    tabled = rowSums(seqtab),
+                    nochim = rowSums(seqtab.nochim))
 rownames(track) <- sample.names
-head(track)
-
-
+track
 
 ###########################################
 ### make output files (FASTA and abundance matrix) for the merged reads
 ############################################
 
+# Note to Emily- these variable names should have an explanation
+# I.e. how will each be used?
 mergedbarC =  paste("merged_", barC , sep='')
 mergedbarCseqnum = paste("merged_", barC , "_seq_number", sep = '')
 nochime_fname.fasta = paste(path,"/", "nochim_merged",barC,".fasta", sep='')
@@ -144,6 +161,8 @@ write.table(nochim_merged, file = nochime_fname.txt, row.names=FALSE, sep="\t", 
 
 # Make a fasta file out of the sequences in this table
 nochim_merged_seq <- nochim_merged %>% select(merged_CO1_seq_number, sequence)
+
+#
 nochim_merged_seq.fasta = dataframe2fas(nochim_merged_seq, file= nochime_fname.fasta)
 
 
@@ -168,6 +187,11 @@ pairedsum.unmerged.table <- try %>% select(id = .id, forward, reverse, abundance
 
 mine_unmerged <- function(df, seqs_list, forward_or_reverse) {
   seq_obj <- seqs_list[[(df[1])]]
+  
+  # Make sure nothing silly is happening
+  if (!(forward_or_reverse %in% c("forward","reverse"))){
+    stop("forward_or_reverse must be specified as 'forward' or 'reverse'")
+  }
   
   if (forward_or_reverse == "forward") {
     seq <- seq_obj$sequence[as.numeric(df[2])]
@@ -195,9 +219,8 @@ pairedsum.unmerged.table$lengthR <- nchar(gsub("[a-z]","",pairedsum.unmerged.tab
 pairedsum.unmerged.table$totalseq <- pairedsum.unmerged.table$lengthF + pairedsum.unmerged.table$lengthR
 
 # add expected length of amplicon
-pairedsum.unmerged.table$expected_amplicon <- barC_length
-pairedsum.unmerged.table$keep[pairedsum.unmerged.table$totalseq>=pairedsum.unmerged.table$expected_amplicon] <- FALSE
-pairedsum.unmerged.table$keep[pairedsum.unmerged.table$totalseq<pairedsum.unmerged.table$expected_amplicon] <- TRUE
+pairedsum.unmerged.table$keep2[pairedsum.unmerged.table$totalseq>=barC_length] <- FALSE
+pairedsum.unmerged.table$keep2[pairedsum.unmerged.table$totalseq<barC_length] <- TRUE
 
 # Get the reverse complement of the R sequence
 pairedsum.unmerged.table$sequenceRc <- sapply(sapply(sapply(pairedsum.unmerged.table$sequenceR, DNAString), reverseComplement), toString)
