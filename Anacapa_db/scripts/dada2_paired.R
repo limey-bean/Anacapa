65e0ae3553ffe6c6a2d03046ef82e8d0bb3ac0b6
Gaurav <gaurav.kandlikar@gmail.com>
2017-11-08 12:40:31 -0800

    comming out package installation chunk- does weird stuff on hoffman

--- Anacapa_db/scripts/dada2_paired.R
+++ Anacapa_db/scripts/dada2_paired.R
@@ -15,19 +15,19 @@ barC_length = "500"
 
 # Install packages that are not currently installed -----
 # This chunk may need attention, putting it here as a starting point - gsk
-.cran_packages  <-  c("ggplot2", "plyr", "dplyr","seqRFLP", "reshape2", "tibble")
-.bioc_packages <- c("phyloseq", "genefilter", "impute", "Biostrings", "dada2")
-
-.inst <- .cran_packages %in% installed.packages()
-if (any(!.inst)) {
-  install.packages(.cran_packages[!.inst], repos = "http://cran.rstudio.com/")
-}
-
-.inst <- .bioc_packages %in% installed.packages()
-if (any(!.inst)) {
-  source("http://bioconductor.org/biocLite.R")
-  biocLite(.bioc_packages[!.inst])
-}
+# .cran_packages  <-  c("ggplot2", "plyr", "dplyr","seqRFLP", "reshape2", "tibble")
+# .bioc_packages <- c("phyloseq", "genefilter", "impute", "Biostrings", "dada2")
+# 
+# .inst <- .cran_packages %in% installed.packages()
+# if (any(!.inst)) {
+#   install.packages(.cran_packages[!.inst], repos = "http://cran.rstudio.com/")
+# }
+# 
+# .inst <- .bioc_packages %in% installed.packages()
+# if (any(!.inst)) {
+#   source("http://bioconductor.org/biocLite.R")
+#   biocLite(.bioc_packages[!.inst])
+# }
 ####################################################################################### process paired end reads
 
 library("dada2")
