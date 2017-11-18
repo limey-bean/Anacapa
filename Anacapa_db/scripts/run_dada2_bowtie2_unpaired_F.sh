b4a4d807c65cfae7cd12511db2a27ed096e560ac
Emily Curd <eecurd@g.ucla.edu>
2017-11-18 12:49:56 -0800

    added bowtie 2 summary script

--- Anacapa_db/scripts/run_dada2_bowtie2_unpaired_F.sh
+++ Anacapa_db/scripts/run_dada2_bowtie2_unpaired_F.sh
@@ -37,25 +37,27 @@ ${GCC}
 ##### add the single and paired bowtie 2 files to different folders. Turn the following code into a for loop for the single bowtie2 reads
 
 
-Rscript  --vanilla ${DB}/scripts/dada2_forward_only.R ${MB} ${OUT} 
+Rscript  --vanilla ${DB}/scripts/dada2_reverse_only.R ${MB} ${OUT} 
 echo "moving on"
 
 
-rm -r ${OUT}/unpaired_F/${MB}/filtered
+
+rm -r ${OUT}/unpaired_R/${MB}/filtered
 
 ########################################
 # Run bowtie2 on merged paired reads
 ########################################
 
 ### end to end mode
-bowtie2 -x ${DB}/${MB}/${MB}_bowtie2_database/${MB}_bowtie2_index  -f -U ${OUT}/${MB}/dada2_bowtie2/unpaired_F/nochim_forward${MB}.fasta -S ${OUT}/${MB}/dada2_bowtie2/unpaired_F/nochim_forward_only${MB}_end_to_end.sam --no-hd --no-sq --very-sensitive --end-to-end --no-unal -p 120 -k 100 --un ${OUT}/${MB}/dada2_bowtie2/unpaired_F/nochim_forward${MB}_end_to_end_reject.fasta --omit-sec-seq
+bowtie2 -x ${DB}/${MB}/${MB}_bowtie2_database/${MB}_bowtie2_index  -f -U ${OUT}/${MB}/dada2_bowtie2/unpaired_R/nochim_reverse${MB}.fasta -S ${OUT}/${MB}/dada2_bowtie2/unpaired_R/nochim_reverse_only${MB}_end_to_end.sam --no-hd --no-sq --very-sensitive --end-to-end --no-unal -p 120 -k 100 --un ${OUT}/${MB}/dada2_bowtie2/unpaired_R/nochim_reverse${MB}_end_to_end_reject.fasta --omit-sec-seq
 
 ### local mode
 
-bowtie2 -x ${DB}/${MB}/${MB}_bowtie2_database/${MB}_bowtie2_index  -f -U ${OUT}/${MB}/dada2_bowtie2/unpaired_F/nochim_forward${MB}_end_to_end_reject.fasta -S ${OUT}/${MB}/dada2_bowtie2/unpaired_F/nochim_forward_only${MB}_local.sam --no-hd --no-sq -D 20 --very-sensitive --local --no-unal -p 120 -k 100 --un ${OUT}/${MB}/dada2_bowtie2/unpaired_F/nochim_forward${MB}_end_to_end_and_local_reject.fasta --omit-sec-seq
+bowtie2 -x ${DB}/${MB}/${MB}_bowtie2_database/${MB}_bowtie2_index  -f -U ${OUT}/${MB}/dada2_bowtie2/unpaired_R/nochim_reverse${MB}_end_to_end_reject.fasta -S ${OUT}/${MB}/dada2_bowtie2/unpaired_R/nochim_reverse_only${MB}_local.sam --no-hd --no-sq --very-sensitive --local --no-unal -p 120 -k 100 --un ${OUT}/${MB}/dada2_bowtie2/unpaired_R/nochim_reverse${MB}_end_to_end_and_local_reject.fasta --omit-sec-seq
+
 
 ########################################
 # Summarize bowtie2 runs and append to dada2 output
 ########################################
 
-python ${DB}/scripts/append_bowtie_to_summary.py ${OUT}/${MB}/dada2_bowtie2/unpaired_F/nochim_forward${MB}.txt ${DB}/${MB}/${MB}_fasta_and_taxonomy/${MB}_taxonomy.txt ${OUT}/${MB}/dada2_bowtie2/unpaired_F/nochim_forward_only${MB}_local.sam  ${OUT}/${MB}/dada2_bowtie2/unpaired_F/nochim_forward_only${MB}_end_to_end.sam
+python ${DB}/scripts/append_bowtie_to_summary.py ${OUT}/${MB}/dada2_bowtie2/unpaired_R/nochim_reverse${MB}.txt ${DB}/${MB}/${MB}_fasta_and_taxonomy/${MB}_taxonomy.txt ${OUT}/${MB}/dada2_bowtie2/unpaired_R/nochim_reverse_only${MB}_local.sam  ${OUT}/${MB}/dada2_bowtie2/unpaired_R/nochim_reverse_only${MB}_end_to_end.sam
