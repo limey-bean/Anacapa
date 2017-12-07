732cdd683c3e02acfd4f678abd2b073c26730ac1
Gaurav <gaurav.kandlikar@gmail.com>
2017-12-06 17:08:03 -0800

    uncommenting package management chunk, works fine for gsk

--- Anacapa_db/scripts/dada2_unified_script.R
+++ Anacapa_db/scripts/dada2_unified_script.R
@@ -51,35 +51,35 @@ if (file.access(path, mode = 2) != 0) {
 
 # Manage packages -----
 
-# 1. Download packages from CRAN
-# .cran_packages  <-  c("ggplot2", "plyr", "dplyr","seqRFLP", "reshape2", "tibble", "devtools", "Matrix", "mgcv")
-# .inst <- .cran_packages %in% installed.packages()
-# if (any(!.inst)) {
-#   install.packages(.cran_packages[!.inst], repos = "http://cran.rstudio.com/")
-# }
-# 
-# # 2. Download packages from biocLite
-# .bioc_packages <- c("phyloseq", "genefilter", "impute", "Biostrings")
-# .inst <- .bioc_packages %in% installed.packages()
-# if (any(!.inst)) {
-#   source("http://bioconductor.org/biocLite.R")
-#   biocLite(.bioc_packages[!.inst])
-# }
-# 
-# .dada_version = "1.6.0"
-# .dada_version_gh = "v1.6"
-# if("dada2" %in% installed.packages()){
-#   if(packageVersion("dada2") == .dada_version) {
-#     cat("congrats, right version of dada2")
-#   } else {
-#     devtools::install_github("benjjneb/dada2", ref=.dada_version_gh)
-#   }
-# }
-# 
-# if(!("dada2" %in% installed.packages())){
-#   # if the user doesn't have dada2 installed, install version 1.6 from github
-#   devtools::install_github("benjjneb/dada2", ref=.dada_version_gh)
-# }
+1. Download packages from CRAN
+.cran_packages  <-  c("ggplot2", "plyr", "dplyr","seqRFLP", "reshape2", "tibble", "devtools", "Matrix", "mgcv")
+.inst <- .cran_packages %in% installed.packages()
+if (any(!.inst)) {
+  install.packages(.cran_packages[!.inst], repos = "http://cran.rstudio.com/")
+}
+
+# 2. Download packages from biocLite
+.bioc_packages <- c("phyloseq", "genefilter", "impute", "Biostrings")
+.inst <- .bioc_packages %in% installed.packages()
+if (any(!.inst)) {
+  source("http://bioconductor.org/biocLite.R")
+  biocLite(.bioc_packages[!.inst])
+}
+
+.dada_version = "1.6.0"
+.dada_version_gh = "v1.6"
+if("dada2" %in% installed.packages()){
+  if(packageVersion("dada2") == .dada_version) {
+    cat("congrats, right version of dada2")
+  } else {
+    devtools::install_github("benjjneb/dada2", ref=.dada_version_gh)
+  }
+}
+
+if(!("dada2" %in% installed.packages())){
+  # if the user doesn't have dada2 installed, install version 1.6 from github
+  devtools::install_github("benjjneb/dada2", ref=.dada_version_gh)
+}
 
 library("dada2")
 cat(paste("dada2 package version:", packageVersion("dada2")))
