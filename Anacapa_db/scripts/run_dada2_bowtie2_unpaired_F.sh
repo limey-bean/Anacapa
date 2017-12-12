483c275f18c9bc7ea49093a9d4565a53558c9d03
Emily Curd <eecurd@g.ucla.edu>
2017-12-12 09:22:45 -0800

    Update run_dada2_bowtie2_unpaired_F.sh

--- Anacapa_db/scripts/run_dada2_bowtie2_unpaired_F.sh
+++ Anacapa_db/scripts/run_dada2_bowtie2_unpaired_F.sh
@@ -48,11 +48,11 @@ rm -r ${OUT}/unpaired_F/${MB}/filtered
 ########################################
 
 ### end to end mode
-bowtie2 -x ${DB}/${MB}/${MB}_bowtie2_database/${MB}_bowtie2_index  -f -U ${OUT}/${MB}/dada2_bowtie2/unpaired_F/nochim_forward${MB}.fasta -S ${OUT}/${MB}/dada2_bowtie2/unpaired_F/nochim_forward_only${MB}_end_to_end.sam --no-hd --no-sq --very-sensitive --end-to-end --no-unal -p 120 -k 100 --un ${OUT}/${MB}/dada2_bowtie2/unpaired_F/nochim_forward${MB}_end_to_end_reject.fasta --omit-sec-seq
+bowtie2 -x ${DB}/${MB}/${MB}_bowtie2_database/${MB}_bowtie2_index  -f -U ${OUT}/${MB}/dada2_bowtie2/unpaired_F/nochim_forward${MB}.fasta -S ${OUT}/${MB}/dada2_bowtie2/unpaired_F/nochim_forward_only${MB}_end_to_end.sam --no-hd --no-sq --very-sensitive --end-to-end --no-unal -p 120 -k 100 --un ${OUT}/${MB}/dada2_bowtie2/unpaired_F/nochim_forward${MB}_end_to_end_reject.fasta
 
 ### local mode
 
-bowtie2 -x ${DB}/${MB}/${MB}_bowtie2_database/${MB}_bowtie2_index  -f -U ${OUT}/${MB}/dada2_bowtie2/unpaired_F/nochim_forward${MB}_end_to_end_reject.fasta -S ${OUT}/${MB}/dada2_bowtie2/unpaired_F/nochim_forward_only${MB}_local.sam --no-hd --no-sq -D 20 --very-sensitive --local --no-unal -p 120 -k 100 --un ${OUT}/${MB}/dada2_bowtie2/unpaired_F/nochim_forward${MB}_end_to_end_and_local_reject.fasta --omit-sec-seq
+bowtie2 -x ${DB}/${MB}/${MB}_bowtie2_database/${MB}_bowtie2_index  -f -U ${OUT}/${MB}/dada2_bowtie2/unpaired_F/nochim_forward${MB}_end_to_end_reject.fasta -S ${OUT}/${MB}/dada2_bowtie2/unpaired_F/nochim_forward_only${MB}_local.sam --no-hd --no-sq -D 20 --very-sensitive --local --no-unal -p 120 -k 100 --un ${OUT}/${MB}/dada2_bowtie2/unpaired_F/nochim_forward${MB}_end_to_end_and_local_reject.fasta
 
 ########################################
 # Summarize bowtie2 runs and append to dada2 output
