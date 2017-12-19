8ba0d6688d44c90e5d365c01b9ac7d618cf9fd17
Emily Curd <eecurd@g.ucla.edu>
2017-12-19 06:38:34 -0800

    Update anacapa_QC_dada2.sh

--- Anacapa_db/scripts/anacapa_QC_dada2.sh
+++ Anacapa_db/scripts/anacapa_QC_dada2.sh
@@ -57,7 +57,6 @@ source $DB/scripts/anacapa_config.sh # edit for proper configuration
 ${MODULE_SOURCE} # use if you need to load modules from an HPC
 ${FASTX_TOOLKIT} #load fastx_toolkit
 ${ANACONDA_PYTHON} #load anaconda/python2-4.2
-${PERL} #load perl
 ${ATS} #load ATS, Hoffman2 specific module for managing submitted jobs.
 date
 ###
