2aab198bd0a50cef87aa6f5663cb1fe807cada33
Emily Curd <eecurd@g.ucla.edu>
2017-12-21 11:43:48 -0800

    Update anacapa_bowtie2_blca.sh

--- Anacapa_db/scripts/anacapa_bowtie2_blca.sh
+++ Anacapa_db/scripts/anacapa_bowtie2_blca.sh
@@ -61,7 +61,7 @@ do
     # generate runlogs that you can submit at any time!
     printf "#!/bin/bash\n#$ -l h_rt=20:00:00,h_data=48G\n#$ -N bowtie2_${j}_blca\n#$ -cwd\n#$ -m bea\n#$ -M ${UN}\n#$ -o ${OUT}/Run_info/hoffman2/run_logs/${j}__bowtie2_blca_$JOB_ID.out\n#$ -e ${OUT}/Run_info/hoffman2/run_logs/${j}_bowtie2_blca_$JOB_ID.err \n\necho _BEGIN_ [run_bowtie2_blca_paired.sh]: `date`\n\nsh ${DB}/scripts/run_bowtie2_blca.sh  -o ${OUT} -d ${DB} -m ${j}\n\necho _END_ [run_bowtie2_blca.sh]" >> ${OUT}/Run_info/hoffman2/run_scripts/${j}_bowtie2_blca_job.sh
     echo ''
-    #qsub ${OUT}/Run_info/hoffman2/run_scripts/${j}_bowtie2_blca_job.sh
+    qsub ${OUT}/Run_info/hoffman2/run_scripts/${j}_bowtie2_blca_job.sh
  fi
 done
 date
