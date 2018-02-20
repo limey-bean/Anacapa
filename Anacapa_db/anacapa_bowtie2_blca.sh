a01a8aaae10c395e973797214212ee84e1aeb1e0
Max Ogden <max@maxogden.com>
2018-02-19 18:07:07 -0800

    use a template file for qsub job file templates

--- Anacapa_db/anacapa_bowtie2_blca.sh
+++ Anacapa_db/anacapa_bowtie2_blca.sh
@@ -35,7 +35,7 @@ done
 # location of the config and var files
 source $DB/scripts/anacapa_vars_nextV.sh  # edit to change variables and parameters
 source $DB/scripts/anacapa_config.sh # edit for proper configuration
-
+source $DB/scripts/anacapa_qsub_templates.sh # edit to change qsub job file format
 
 ##load modules / software
 ${MODULE_SOURCE} # use if you need to load modules from an HPC
@@ -58,7 +58,7 @@ do
     #make folders for the metabarcode specific output of dada2 and bowtie2
  	echo "${j}"
     # generate runlogs that you can submit at any time!
-    printf "#!/bin/bash\n#$ -l highp,h_rt=210:00:00,h_data=20G\n#$ -N bowtie2_${j}_blca\n#$ -cwd\n#$ -m bea\n#$ -M ${UN}\n#$ -o ${OUT}/Run_info/hoffman2/run_logs/${j}_bowtie2_blca_$JOB_ID.out\n#$ -e ${OUT}/Run_info/hoffman2/run_logs/${j}_bowtie2_blca_$JOB_ID.err \n\necho _BEGIN_ [run_bowtie2_blca_paired.sh]: `date`\n\nbash ${DB}/scripts/run_bowtie2_blca.sh  -o ${OUT} -d ${DB} -m ${j} -u ${UN}\n\necho _END_ [run_bowtie2_blca.sh]" >> ${OUT}/Run_info/hoffman2/run_scripts/${j}_bowtie2_blca_job.sh
+    printf $BOWTIE2_BLCA_PAIRED_TEMPLATE >> ${OUT}/Run_info/hoffman2/run_scripts/${j}_bowtie2_blca_job.sh
     echo ''
     qsub ${OUT}/Run_info/hoffman2/run_scripts/${j}_bowtie2_blca_job.sh
     if [ "${LOCALMODE}" = "TRUE"  ]  # if you are running locally (no hoffman2) you can run these jobs one after the other.
--- Anacapa_db/scripts/run_bowtie2_blca.sh
+++ Anacapa_db/scripts/run_bowtie2_blca.sh
@@ -38,7 +38,7 @@ done
 # location of the config and var files
 source $DB/scripts/anacapa_vars_nextV.sh  # edit to change variables and parameters
 source $DB/scripts/anacapa_config.sh # edit for proper configuration
-
+source $DB/scripts/anacapa_qsub_templates.sh # edit to change qsub job file format
 
 ##load modules / software
 ${MODULE_SOURCE} # use if you need to load modules from an HPC
@@ -136,7 +136,7 @@ else
     then
  	   echo "${str}"
      # generate runlogs that you can submit at any time!
-     printf "#!/bin/bash\n#$ -l highp,h_rt=200:00:00,h_data=20G\n#$ -N bowtie2_${j}_blca\n#$ -cwd\n#$ -m bea\n#$ -M ${UN} \n#$ -o ${OUT}/Run_info/hoffman2/run_logs/${j}_blca_$JOB_ID.out\n#$ -e ${OUT}/Run_info/hoffman2/run_logs/${j}_blca_$JOB_ID.err \n\necho _BEGIN_ [run_blca.sh]: `date`\n\nbash ${DB}/scripts/run_blca.sh -o ${OUT} -d ${DB} -m ${MB} -f ${str} \n\necho _END_ [run_blca.sh]" >> ${OUT}/Run_info/hoffman2/run_scripts/${j}_blca_job.sh
+     printf $BLCA_TEMPLATE >> ${OUT}/Run_info/hoffman2/run_scripts/${j}_blca_job.sh
      echo ''
      qsub ${OUT}/Run_info/hoffman2/run_scripts/${j}_blca_job.sh
      echo "if a blca job(s) fails you can find the job submission file in ${OUT}/Run_info/hoffman2/run_scripts"
