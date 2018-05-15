5cdf5e678e145e526a352fb9dff36dc31d321583
EMily Curd <limeybean@EMilys-MacBook-Pro-6.local>
2018-05-15 13:44:29 -0700

    clean up run_dada2.sh

--- Anacapa_db/scripts/run_dada2.sh
+++ Anacapa_db/scripts/run_dada2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ### this script is run as follows
-# ~/Anacapa_db/scripts/run_bowtie2_make_3_Sfolders.sh -o <out_dir> -d <database_directory> -m <metabarcode name> -e <Minimum length for paired F and R reads to merge> -b <min ASV read count acceptable>
+# ~/Anacapa_db/scripts/run_dada2.sh -o <out_dir> -d <database_directory> -m <metabarcode name> -e <Minimum length for paired F and R reads to merge> -b <min ASV read count acceptable>
 OUT=""
 DB=""
 MB=""
@@ -33,7 +33,7 @@ source $DB/scripts/anacapa_config.sh
 source ${MIN_MERGE_LENGTH}
 
 
-##### add the single and paired bowtie 2 files to different folders. Turn the following code into a for loop for the single bowtie2 reads
+### grab the minimum length for metabarcode reads to merge from the config file.
 length_var=LENGTH_${MB}
 length=${!length_var}
 
@@ -45,11 +45,6 @@ echo "${MIN_ASV}"
 
 ##load module
 ${MODULE_SOURCE} # use if you need to load modules from an HPC
-
-${BOWTIE2} #load bowtie2
-${ANACONDA_PYTHON} # load anaconda python
-${PYTHON} # load python
-
 
 #### critical or the dependency 'RcppParallel' will not install
 ${R} &> ${OUT}/Run_info/dada2_out/dada2_out_${TYP}
